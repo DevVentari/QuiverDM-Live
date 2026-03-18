3c372254ce961be9a48c4e5bfb414f0e9b23ce95
mail <dev@blakewales.au>
2026-03-19 00:47:17 +1100

    feat: add homebrew component barrel exports

---
+++ src/components/homebrew/index.ts
@@ -0,0 +1,3 @@
+export { SpellCard, type SpellCardData } from './SpellCard';
+export { MonsterStatBlock, type MonsterStatBlockData, type MonsterAction, type MonsterTrait } from './MonsterStatBlock';
+export { MagicItemCard, type MagicItemCardData } from './MagicItemCard';
